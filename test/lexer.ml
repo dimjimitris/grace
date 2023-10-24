66c305e8939f1b7ce9aeae9401ef72cdd23377c5
Georgios-Alexios Kapetanakis <george.kapetanakis.gk@gmail.com>
2023-10-24 13:07:39 +0000

    changed error handling to be more explicit

--- test/lexer.ml
+++ test/lexer.ml
@@ -1,6 +1,8 @@
+open Grace_lib
+
 let () =
   let dirname = "programs/" in
-  let filenames = [ "program1.grc"; "program2.grc" ] in
+  let filenames = [ "program1.grc"; "program2.grc"; "program3.grc"; "program4.grc" ] in
   let test filename =
     let chan = open_in (dirname ^ filename) in
     let lexbuf = Lexing.from_channel chan in
@@ -8,14 +10,14 @@ let () =
     let rec tokenize_and_print () =
       try
         (* call the lexer's 'token' rule (tokenize input) *)
-        let token = Grace_lib.Lexer.token lexbuf in
+        let token = Lexer.token lexbuf in
         (* print found tokens *)
         match token with
-        | Grace_lib.Tokens.EOF -> ()
+        | Tokens.EOF -> ()
         | _ ->
-            print_endline (Grace_lib.Lexer_utils.string_of_token token);
+            print_endline (Lexer_utils.string_of_token token);
             tokenize_and_print ()
-      with err -> Grace_lib.Error.pr_error err
+      with Error.Lexing_error (loc, msg) -> Error.pr_lexing_error (loc, msg)
     in
     print_endline filename;
     tokenize_and_print ()
--- test/parser.ml
+++ test/parser.ml
@@ -1,19 +1,22 @@
+open Grace_lib
+
 let () =
   let dirname = "programs/" in
-  let filenames = [ "program3.grc" ] in
+  let filenames = [ "program1.grc"; "program2.grc"; "program3.grc"; "program4.grc" ] in
   let test filename =
     let chan = open_in (dirname ^ filename) in
     let lexbuf = Lexing.from_channel chan in
     let () = Lexing.set_filename lexbuf filename in
     try
-      let ast_node = Grace_lib.Parser.program Grace_lib.Lexer.token lexbuf in
-      print_string
-        (Grace_lib.Print_ast.pr_func_def "" true
-           (Grace_lib.Ast.get_node ast_node))
-    with err ->
-      Grace_lib.Error.pr_error err;
-      print_endline (
-      Grace_lib.Print_symbol.pr_symbol_table "" true
-        Grace_lib.Gift.tbl)
+      let ast_node = Parser.program Grace_lib.Lexer.token lexbuf in
+      print_string (Print_ast.pr_program (Ast.get_node ast_node))
+    with
+      | Error.Lexing_error (loc, msg) ->
+          Error.pr_lexing_error (loc, msg);
+          print_endline (Print_symbol.pr_symbol_table "" true Gift.tbl)
+      | Error.Semantic_error (loc, msg) -> Error.pr_semantic_error (loc, msg);
+          print_endline (Print_symbol.pr_symbol_table "" true Gift.tbl)
+      | Error.Symbol_table_error (loc, msg) -> Error.pr_symbol_table_error (loc, msg);
+          print_endline (Print_symbol.pr_symbol_table "" true Gift.tbl)
   in
   List.iter test filenames
